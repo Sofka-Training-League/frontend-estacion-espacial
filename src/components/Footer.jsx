1b4a2f6d6850ca56d3b02d1fbe681e9de27db428
Duhan Renteria <derh7280@hotmail.com>
2022-07-15 20:37:56 -0500

    Componente footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/footer.css'
+import logo from '../assets/image/logo.png'
+
+export const Footer= () => {
+  
+        return (
+            <footer>
+            <div className="footer">
+                <div className="box__copyright">
+                    <hr />
+                    <p><b className="hr">ESTESPA - </b>Todos los derechos reservados © 2022 <b className="hr">Drenthe</b></p>
+                </div>
+            </div>
+            </footer>
+        )
+}
